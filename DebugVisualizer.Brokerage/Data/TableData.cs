743f5a744fb7b535b3d5904b3022ab9a635c5eb0
Henning Dieterichs <henning.dieterichs@jetbrains.com>
2020-09-22 19:26:32 +0200

    Renamed List -> Table.

---
+++ DebugVisualizer.Brokerage/Data/TableData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DebugVisualizer.Brokerage.Data
+{
+    public sealed class TableData : VisualizationData
+    {
+        public override string[] Tags => new[] {"table"};
+
+        [JsonProperty("rows")] public List<Dictionary<string, object>> Items { get; set; }
+
+        public TableData() : this(new List<Dictionary<string, object>>())
+        {
+        }
+
+        public TableData(List<Dictionary<string, object>> items)
+        {
+            this.Items = items;
+        }
+    }
+}
